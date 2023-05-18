b3ef60e02c8605739aeda18178e6b93ae04c7b09
Urusha Shahi <urushashahi@lftechnology.com>
2023-05-18 17:43:56 +0545

    Refactor practitioner

--- backend/src/api/validation/Practitioner/CreateValidator.js
+++ backend/src/api/validation/Practitioner/CreateValidator.js
@@ -2,15 +2,15 @@ const Joi = require('joi');
 const validate = require('./../validate');
 
 const createPatientSchema = Joi.object({
-    first_name: Joi.string().required(),
-    last_name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    contact: Joi.string().required(),
-    dob: Joi.date().required(),
-    start_time: Joi.required(),
-    end_time: Joi.required(),
-    working_days: Joi.array().items(Joi.number()).required(),
-    is_icu_specialist: Joi.boolean()
+  first_name: Joi.string().required(),
+  last_name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  contact: Joi.string().required(),
+  dob: Joi.date().required(),
+  start_time: Joi.required(),
+  end_time: Joi.required(),
+  working_days: Joi.array().items(Joi.number()).required(),
+  is_ICU_Specialist: Joi.boolean(),
 });
 
 module.exports = validate(createPatientSchema);
